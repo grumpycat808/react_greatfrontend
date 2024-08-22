df772bf69b52b99e75ad272989624fc0f908a2be
Catherine Li <catherineli808@gmail.com>
2024-08-22 12:10:10 -0700

    Completed users db

--- src/UsersDatabase.js
+++ src/UsersDatabase.js
@@ -7,31 +7,59 @@ function UsersDatabase(props) {
     const [names, setNames] = useState(staticNames)
     const [searchStr, setSearchStr] = useState('')
     const [selectedName, setSelectedName] = useState('Jennifer Aniston')
-
+    const [newFname, setNewFname] = useState('')
+    const [newLname, setNewLname] = useState('')
     useEffect(() => {
         let filteredNames = [...staticNames]
-        filteredNames = filteredNames.filter((name) =>
-            name.includes(searchStr),
-        )
-        
+        filteredNames = filteredNames.filter((name) => name.includes(searchStr))
+
         setNames(filteredNames)
     }, [searchStr])
+
+    useEffect(() => {
+        setNewFname(getFname())
+        setNewLname(getLname())
+    }, [selectedName])
     const filterOptions = (searchString) => {
         setSearchStr(searchString)
-        
+    }
+    const handleUpdate = () => {
+        const indexOfOG = names.indexOf(selectedName);
+        const newNamesList = [...names];
+        newNamesList[indexOfOG] = `${newFname} ${newLname}`;
+        setNames(newNamesList)
+        setNewFname('');
+        setNewLname('')
     }
 
+    const handleCreate = () => {
+        if(selectedName !== '') return;
+
+        if(newFname === '' || newLname === '') return;
+        setNames([...names, `${newFname} ${newLname}`])
+        setNewFname('');
+        setNewLname('')
+    }
     const handleDelete = () => {
-        let filtered = [...names];
-        filtered = filtered.filter((name) => name !== selectedName);console.log('filtered', filtered)
+        let filtered = [...names]
+        filtered = filtered.filter((name) => name !== selectedName)
+        console.log('filtered', filtered)
         setNames(filtered)
     }
 
     const handleSelect = (value) => {
-        
         setSelectedName(value)
     }
 
+    const handleUpdateFname = (value) => {
+        setNewFname(value)
+    }
+
+    const handleUpdateLname = (value) => {
+        setNewLname(value)
+    }
+
+    const handleCancel = () => setSelectedName('')
     const getFname = () => selectedName && selectedName.split(' ')[0]
     const getLname = () => selectedName && selectedName.split(' ')[1]
     return (
@@ -71,23 +99,23 @@ function UsersDatabase(props) {
                     <input
                         type="text"
                         name="fname"
-                        value={getFname()}
-                        onChange={(e) => console.log(e.target.value)}
+                        value={newFname}
+                        onChange={(e) => handleUpdateFname(e.target.value)}
                     ></input>
                     <label htmlFor="lname">Last Name: </label>
                     <input
                         type="text"
                         name="lname"
-                        value={getLname()}
-                        onChange={(e) => console.log(e.target.value)}
+                        value={newLname}
+                        onChange={(e) => handleUpdateLname(e.target.value)}
                     ></input>
                 </form>
             </div>
             <div className="buttons">
-                <button>Create</button>
-                <button>Update</button>
+                <button disabled={selectedName !== '' || newFname === '' || newLname === ''} onClick={handleCreate}>Create</button>
+                <button onClick={handleUpdate}>Update</button>
                 <button onClick={handleDelete}>Delete</button>
-                <button>Cancel</button>
+                <button onClick={handleCancel}>Cancel</button>
             </div>
         </div>
     )
