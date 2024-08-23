852b4b6226dcf8d4330cc5a4fe7c0cd02e75721a
Catherine Li <catherineli808@gmail.com>
2024-08-23 08:16:47 -0700

    Formatting

--- src/BirthyearChart.js
+++ src/BirthyearChart.js
@@ -10,21 +10,19 @@ function BirthyearChart(props) {
         getData()
     }, [])
 
-
     async function getData() {
-        
         try {
-          const response = await fetch(url);
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-      
-          const json = await response.json();
-          console.log(json);
+            const response = await fetch(url)
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`)
+            }
+
+            const json = await response.json()
+            console.log(json)
         } catch (error) {
-          console.error(error.message);
+            console.error(error.message)
         }
-      }
+    }
     return <div></div>
 }
 
---
+++ src/Database.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Database({ data }) {
+    return <div></div>
+}
+
+export default Database
