6fdc5450847010ddd8008664f9e5e23cc0eff7f1
Catherine Li <catherineli808@gmail.com>
2024-08-23 10:01:47 -0700

    making database into a generic component

--- src/App5.js
+++ src/App5.js
@@ -3,14 +3,19 @@ import UsersDatabase from './UsersDatabase'
 import ModalDialog from './ModalDialog'
 import BirthyearChart from './BirthyearChart'
 import DataTable from './DataTable'
-
+import Database from './Database'
+import users from './users.json'
+import houses from './houses.json'
 function App5(props) {
     const [isOpen, setIsOpen] = useState(false)
     const content =
         'One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.'
     return (
         <div className="main">
-            <DataTable></DataTable>
+            <Database
+                data={houses}
+                columns={['id', 'street', 'city', 'state', 'built_year']}
+            ></Database>
             {/* <BirthyearChart></BirthyearChart> */}
             {/* <button onClick={() => setIsOpen(true)}>Show Modal</button> */}
             {/* <ModalDialog title="Modal Title" isOpen={isOpen} content={content} closeModal={() => setIsOpen(false)}></ModalDialog> */}
--- src/Database.js
+++ src/Database.js
@@ -1,7 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Database({ data }) {
-    return <div></div>
+import { v4 } from 'uuid'
+function Database({ columns, data }) {
+
+    const [numRows, setNumRows] = useState(5);
+    const [formattedData, setFormattedData] = useState([...data])
+    const [order, setOrder] = useState(() => {
+        const orderObj = {};
+
+        columns.forEach((col) => orderObj[col] = {ascending : false})
+        console.log(orderObj)
+        return orderObj
+    })
+
+    const getNumPages = () => {
+        return Math.ceil(data.length/numRows)
+    }
+
+    const spliceRows = () => {
+        
+    }
+    const updateSortOrder = (col, ascending) => {
+        
+        if(!formattedData) return;
+        const datatype = typeof formattedData[0][col];
+        const sorted = [...formattedData];
+        switch(datatype) {
+            case 'string':
+                if(ascending) {
+                    sorted.sort((a, b) => a[col].localeCompare(b[col]))
+                } else {
+                    sorted.sort((a, b) => b[col].localeCompare(a[col]))
+                }
+              
+              break;
+            case 'number':
+                if(ascending) {
+                    sorted.sort((a, b) => a[col]- b[col])
+                } else {
+                    sorted.sort((a, b) => b[col]- a[col])
+                }
+              break;
+            default:
+              // code block
+          }
+          let orderOBJ = {...order};
+          orderOBJ[col].ascending = ascending;
+          
+          setOrder(orderOBJ)
+          setFormattedData(sorted)
+    }
+    return (
+        <div>
+            <label>
+                Rows Displayed:
+                <select name="rowsDisplayed" value={numRows} onChange={(e) => setNumRows(e.target.value)}>
+                    <option value={5}>5</option>
+                    <option value={10}>10</option>
+                    <option value={20}>20</option>
+                </select>
+            </label>
+            <table>
+                <thead>
+                    <tr>
+                        {columns.map((col) => (
+                            <th onClick={() => updateSortOrder(col, !order[col].ascending)} key={v4()}>{col.toUpperCase()}</th>
+                        ))}
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {formattedData && formattedData.map((item) => (
+                        <tr key={v4()}>
+                            {columns.map((col) => {
+                                return <td key={v4()}>{item[col]}</td>
+                            })}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Database
