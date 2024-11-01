d2c35bf7980c72fbebcf2522feaa16d3702e7b76
Catherine Li <catherineli808@gmail.com>
2024-10-31 18:27:49 -0700

    Set state race condition

--- src/NestedCheckboxes.js
+++ src/NestedCheckboxes.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 function NestedCheckboxes({ data }) {
     const [selected, setSelected] = useState([])
-    // useEffect(() => {
-    //     console.log("selected", selected)
-
-    // }, [selected])
+    useEffect(() => {
+        // setSelected([...selected, 8])
+    }, [selected])
 
     const handleCheck = (id) => {
         let newSelected = [...selected]
@@ -16,21 +15,46 @@ function NestedCheckboxes({ data }) {
         }
         if(selected.includes(id)) {
             //handle unselect
-            console.log('newSelected', newSelected);
-            console.log('id', id);
-            console.log('children', children);
-            // debugger
+          
             newSelected = newSelected.filter((item) => item !== id && !children.includes(item))
         } else {
             //handle select
             newSelected.push(id);
             newSelected = [...newSelected, ...children]
+            console.log("getParent(id)", getParent(id))
+            
         }
         
         setSelected(newSelected)
+        const parentId = getParent(id);
+            if(id !== parentId) {
+                //Check if we need to select parent
+                if(allChildrenSelected(parentId)) {
+                    newSelected.push(parentId.id);
+                    console.log("newSelected", newSelected)
+
+                    setSelected(newSelected)
+                }
+            }
     }
+    const allChildrenSelected = (id) => {
+        let children = getChildren([id]);
+        let allSelected = true;
 
-
+        console.log('id', id);
+        children = children.filter((child) => !selected.includes(child) && child !== id.id)
+        console.log('children', children);
+        return children.length === 0;
+    }
+    const getParent = (id) => {
+        return data.find((item) => {
+            const children = getChildren([item]);
+            console.log('children', children);
+            console.log('id', id);
+            return children.includes(id);
+        })
+        
+    }
     const findItemById = (dataArray, id) => {
         let returnItem = null;
 
@@ -51,9 +75,9 @@ function NestedCheckboxes({ data }) {
         return returnItem;
     }
 
-    const getChildren = (item, list = []) => {
-        console.log('item', item);
-        item.forEach((cbItem) => {
+    const getChildren = (itemsArray, list = []) => {
+        
+        itemsArray.forEach((cbItem) => {
             if (cbItem.hasOwnProperty('children')) {
                 list.push(cbItem.id)
                 getChildren(cbItem.children, list)
