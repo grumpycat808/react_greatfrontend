bb24bc12ad70bbf56d0765f73ef0540239a6c5d5
Catherine Li <catherineli808@gmail.com>
2024-11-14 10:29:24 -0800

    Completed tic tac toe

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,34 +1,68 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './tic-tac-toe.css'
 function TicTacToe(props) {
-    const board = []
-    const row = []
     const initialState = [
         ['', '', ''],
         ['', '', ''],
         ['', '', ''],
     ]
-    const [action, setAction] = useState('X');
+
+    const [action, setAction] = useState('X')
     const [boardState, setBoardState] = useState(initialState)
+    const checkWinner = () => {
+        for (let index = 0; index < boardState.length; index++) {
+            const value = boardState[index][0]
+            let same = true
+            for (let j = 1; j < boardState[index].length; j++) {
+                if (boardState[index][j] != value) same = false
+            }
+
+            if (same) return value
+        }
+
+        for (let index = 0; index < 3; index++) {
+            const element = boardState[0][index]
+            let same = true
+            for (let j = 0; j < 3; j++) {
+                if (boardState[j][index] != element) same = false
+            }
+            if (same) return element;
+        }
+
+        if(boardState[0][0] === boardState[1][1] && boardState[0][0] === boardState[2][2]) return boardState[0][0];
+
+        if(boardState[2][0] === boardState[1][1] && boardState[0][0] === boardState[0][2]) return boardState[0][0];
+        return '';
+    }
+    useEffect(() => {
+        if(checkWinner() !== "") {
+            console.log("Winner: " + checkWinner());
+        }
+    }, [boardState])
+
     const handleClick = (row, column) => {
-        const copy = structuredClone(boardState);
+        if (boardState[row][column] !== '') return
+        console.log('row', row)
+        console.log('column', column)
+        const copy = structuredClone(boardState)
+
+        copy[row][column] = action
+        setAction(action === 'X' ? 'O' : 'X')
+        setBoardState(copy)
         
-        copy[row][column] = action;
-        setAction(action === 'X' ? 'O' : 'X');
-        setBoardState(copy);
     }
     return (
         <div className="board">
             {boardState.map((row, rowIndex) => {
+                console.log(initialState[0][1])
                 return (
                     <div key={rowIndex} className="row">
                         {row.map((item, index) => {
-                           
                             return (
                                 <div
                                     key={index}
                                     className="square"
-                                    onClick={() => handleClick(index, rowIndex)}
+                                    onClick={() => handleClick(rowIndex, index)}
                                 >
                                     {item}
                                 </div>
