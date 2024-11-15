434204a47a79747ab5cc9773f3dce0f77dcdb84d
Catherine Li <catherineli808@gmail.com>
2024-11-15 09:57:18 -0800

    hide emojis

--- src/Board.js
+++ src/Board.js
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from 'react'
 import './whack-a-mole.css'
 import {shuffle} from './random-numbers'
 function Board({ emojis }) {
-    console.log("emoji prop", emojis)
+    const [randomEmojis, setRandomEmojis] = useState([]);
+    const [board, setBoard] = useState({});
     useEffect(() => {
         shuffle(emojis);
-        setRandomEmojis(emojis)
+        setRandomEmojis(emojis);
+        const boardCopy = {};
+        emojis.forEach(element => {
+            boardCopy[element] = 'hidden'
+        });
+
+        setBoard(boardCopy);
     }, [emojis])
 
-    const [randomEmojis, setRandomEmojis] = useState([]);
     return (
         <div className="board">
             {randomEmojis.map((square, index) => (
-                <div key={index} className="square">
-                    {square}
+                <div key={index} className={'square'}>
+                    <span className={board[square] === 'hidden' ? "emoji hidden" : 'emoji'}>{square}</span>
+                    
                 </div>
             ))}
         </div>
