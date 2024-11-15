c18716d4bac498db00c289295b2e9527d9abae48
Catherine Li <catherineli808@gmail.com>
2024-11-15 08:13:01 -0800

    Reset button

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -6,73 +6,60 @@ function TicTacToe(props) {
         ['', '', ''],
         ['', '', ''],
     ]
+    const [boardState, setBoardState] = useState(initialState);
+    const [action, setAction] = useState('X');
+    const [winner, setWinner] = useState('');
+    useEffect(() => {
+        const currentWinner = checkWinner();
+        if(currentWinner !== '') console.log("Winner: " + currentWinner)
+            setWinner(currentWinner);
+    }, [boardState])
+    //What should be used as the key?
 
-    const [action, setAction] = useState('X')
-    const [boardState, setBoardState] = useState(initialState)
+    const handleReset = () => {
+        setBoardState(initialState);
+        setWinner('');
+    }
     const checkWinner = () => {
         for (let index = 0; index < boardState.length; index++) {
-            const value = boardState[index][0]
-            let same = true
+            let same = true;
+            const value = boardState[index][0];
             for (let j = 1; j < boardState[index].length; j++) {
-                if (boardState[index][j] != value) same = false
+                if(value !== boardState[index][j]) same = false;
             }
-
-            if (same) return value
+            if(same) return value
         }
 
-        for (let index = 0; index < 3; index++) {
-            const element = boardState[0][index]
-            let same = true
-            for (let j = 0; j < 3; j++) {
-                if (boardState[j][index] != element) same = false
+        for (let index = 0; index < boardState[0].length; index++) {
+            let same = true;
+            const value = boardState[0][index];
+            for (let j = 1; j < boardState.length; j++) {
+                if(value !== boardState[j][index]) same = false;
             }
-            if (same) return element;
+            if(same) return value
         }
 
         if(boardState[0][0] === boardState[1][1] && boardState[0][0] === boardState[2][2]) return boardState[0][0];
+        if(boardState[2][0] === boardState[1][1] && boardState[2][0] === boardState[0][2]) return boardState[2][0];
 
-        if(boardState[2][0] === boardState[1][1] && boardState[0][0] === boardState[0][2]) return boardState[0][0];
         return '';
     }
-    useEffect(() => {
-        if(checkWinner() !== "") {
-            console.log("Winner: " + checkWinner());
-        }
-    }, [boardState])
-
-    const handleClick = (row, column) => {
-        if (boardState[row][column] !== '') return
-        console.log('row', row)
-        console.log('column', column)
-        const copy = structuredClone(boardState)
-
-        copy[row][column] = action
-        setAction(action === 'X' ? 'O' : 'X')
-        setBoardState(copy)
+    const handleClick = (row, col) => {
+        if(boardState[row][col] !== '' || winner !== '') return;
         
+        const copy = structuredClone(boardState);
+        copy[row][col] = action;
+        console.log(copy);
+        setBoardState(copy);
+        setAction(action === 'X' ? 'O' : 'X');
     }
-    return (
-        <div className="board">
-            {boardState.map((row, rowIndex) => {
-                console.log(initialState[0][1])
-                return (
-                    <div key={rowIndex} className="row">
-                        {row.map((item, index) => {
-                            return (
-                                <div
-                                    key={index}
-                                    className="square"
-                                    onClick={() => handleClick(rowIndex, index)}
-                                >
-                                    {item}
-                                </div>
-                            )
-                        })}
-                    </div>
-                )
-            })}
-        </div>
-    )
+    const board = boardState.map((row, rowIndex) => {
+        
+        return <div className='row' key={rowIndex}>{row.map((square, colIndex) =>{
+            return  <div key={colIndex} onClick={() => handleClick(rowIndex, colIndex)} className='square'>{square}</div>
+        })}</div>
+    })
+    return <div className='board'>{board}<button onClick={() => handleReset()}>Reset Game</button></div>
 }
 
 export default TicTacToe
