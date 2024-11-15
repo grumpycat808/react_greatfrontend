fdaba380b50c03023ca0cbeafe030461e34dba9d
Catherine Li <catherineli808@gmail.com>
2024-11-15 10:25:45 -0800

    Limit emoji selection to 2

--- src/Board.js
+++ src/Board.js
@@ -1,26 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import './whack-a-mole.css'
-import {shuffle} from './random-numbers'
+import { shuffle } from './random-numbers'
 function Board({ emojis }) {
-    const [randomEmojis, setRandomEmojis] = useState([]);
-    const [board, setBoard] = useState({});
-    useEffect(() => {
-        shuffle(emojis);
-        setRandomEmojis(emojis);
-        const boardCopy = {};
-        emojis.forEach(element => {
-            boardCopy[element] = 'hidden'
-        });
+    const [randomEmojis, setRandomEmojis] = useState([])
+    const [board, setBoard] = useState([]);
 
-        setBoard(boardCopy);
+    const [selected, setSelected] = useState([]);
+    useEffect(() => {
+        shuffle(emojis)
+        setRandomEmojis(emojis)
+        const boardCopy = []
+        emojis.forEach((element, i) => {
+            boardCopy.push({ em: element, index: i, state: 'hidden' })
+        })
+
+        setBoard(boardCopy)
     }, [emojis])
 
+    useEffect(() => {
+     console.log(selected);
+     
+     if(selected.length === 2) {
+        setTimeout(() => {
+            const copy = structuredClone(board)
+            copy[selected[0]].state = 'hidden'
+            copy[selected[1]].state = 'hidden'
+            setBoard(copy)
+        }, 1000)
+     }
+    }, [selected])
+    const handleClick = (index) => {
+
+        if(selected.length >= 2) return;
+        setSelected([...selected, index]);
+        
+        const copy = structuredClone(board)
+        copy[index].state = 'visible'
+
+        setBoard(copy)
+
+        
+    }
     return (
         <div className="board">
             {randomEmojis.map((square, index) => (
-                <div key={index} className={'square'}>
-                    <span className={board[square] === 'hidden' ? "emoji hidden" : 'emoji'}>{square}</span>
-                    
+                <div
+                    onClick={() => handleClick(index)}
+                    key={index}
+                    className={'square'}
+                >
+                    <span
+                        className={
+                            board[index].state === 'hidden'
+                                ? 'emoji hidden'
+                                : 'emoji'
+                        }
+                    >
+                        {square}
+                    </span>
                 </div>
             ))}
         </div>
--- src/random-numbers.js
+++ src/random-numbers.js
@@ -16,19 +16,18 @@ export const generateRandomNumbers = (num, upper) => {
 // }
 
 export const shuffle = (array) => {
-    let currentIndex = array.length;
-  
+    let currentIndex = array.length
+
     // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--
+
+        // And swap it with the current element.
+        ;[array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ]
     }
-  }
-  
- 
+}
