3626f2ed0abc26a8b784f1cb1be37fe7c29bc0ac
Catherine Li <catherineli808@gmail.com>
2025-03-16 13:05:54 -0700

    Updated handle select

--- src/ticTacToe/TransferList.js
+++ src/ticTacToe/TransferList.js
@@ -2,64 +2,103 @@ import React, { useState } from 'react'
 import './transferList.css'
 function TransferList(props) {
     const [list, setList] = useState({
-        left: ['HTML', 'CSS', 'JavaScript', 'TypeScript'],
-        right: ['React', 'Angular', 'Vue', 'Svelte'],
+        left: [
+            { label: 'HTML', selected: false },
+            { label: 'CSS', selected: false },
+            { label: 'JavaScript', selected: false },
+            { label: 'TypeScript', selected: false },
+        ],
+        right: [
+            { label: 'React', selected: false },
+            { label: 'Angular', selected: false },
+            { label: 'Vue', selected: false },
+            { label: 'Svelte', selected: false },
+        ],
     })
-    const [selected, setSelected] = useState({left: [], right: []})
+   
 
-    const { left, right } = list;
-    
-    const handleSelect = (item, list) => {
-        const selectedCopy = structuredClone(selected);
+    const { left, right } = list
 
-        if(selectedCopy[list].includes(item)) {
-            const filteredList = selectedCopy[list].filter(i => i != item);
-            selectedCopy[list] = filteredList;
-        } else {
-             selectedCopy[list] = [...selectedCopy[list], item];
-        }
-       
-        setSelected(selectedCopy);
+    const handleSelect = (item, side) => {
+        
+        const listCopy = structuredClone(list);
+        const listItem = listCopy[side].find(i => i.label === item);
+        listItem.selected = !listItem.selected
+
+        setList(listCopy)
     }
 
     const handleClick = (direction) => {
+        const listCopy = structuredClone(list)
+        if (direction === 'left') {
+            const itemsToMove = list.right.filter(i => i.selected);
+            listCopy.left = [...listCopy.left, ...itemsToMove];
+            listCopy.right = listCopy.right.filter(
+                // (rightItem) => !selected.right.includes(rightItem),
+            )
+        } else {
+            const itemsToMove = list.left.filter(i => i.selected);
+            listCopy.left = [...listCopy.right, ...itemsToMove];
+            listCopy.left = listCopy.left.filter(
+                // (leftItem) => !selected.left.includes(leftItem),
+            )
+        }
 
+        setList(listCopy)
     }
 
     const handleTransferAll = (direction) => {
-        const listCopy = structuredClone(list);
+        const listCopy = structuredClone(list)
 
-        if(direction === 'left' ){
-            listCopy.left = [...listCopy.left, ...listCopy.right];
-            listCopy.right = [];
+        if (direction === 'left') {
+            listCopy.left = [...listCopy.left, ...listCopy.right]
+            listCopy.right = []
         } else {
-            listCopy.right = [...listCopy.right, ...listCopy.left];
-            listCopy.left = [];
+            listCopy.right = [...listCopy.right, ...listCopy.left]
+            listCopy.left = []
         }
+
+        setList(listCopy)
     }
     return (
         <div>
             <div className="main">
                 <ul>
-                    {left.map((item) => {
-                        return <li>
-                            <input type="checkbox" onChange={() => handleSelect(item, "left")} checked={selected.left.includes(item)}></input>
-                            {item}
-                        </li>
+                    {left.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <input
+                                    type="checkbox"
+                                    onChange={() => handleSelect(item.label, 'left')}
+                                    checked={list.left.find((i) => i.label === item.label).selected}
+                                ></input>
+                                {item.label}
+                            </li>
+                        )
                     })}
                 </ul>
                 <div className="buttons">
-                    <button>{`<<`}</button>
-                    <button>{`<`}</button>
-                    <button>{`>>`}</button>
-                    <button>{`>`}</button>
+                    <button
+                        onClick={() => handleTransferAll('left')}
+                    >{`<<`}</button>
+                    <button onClick={() => handleClick('left')}>{`<`}</button>
+                    <button
+                        onClick={() => handleTransferAll('right')}
+                    >{`>>`}</button>
+                    <button onClick={() => handleClick('right')}>{`>`}</button>
                 </div>
                 <ul>
-                    {right.map((item) => {
-                        return <li>
-                            <input type="checkbox" onChange={() => handleSelect(item, "right")} checked={selected.right.includes(item)}></input>
-                            {item}{' '}
-                        </li>
+                    {right.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <input
+                                    type="checkbox"
+                                    onChange={() => handleSelect(item.label, 'right')}
+                                    checked={list.right.find((i) => i.label === item.label).selected}
+                                ></input>
+                                {item.label}{' '}
+                            </li>
+                        )
                     })}
                 </ul>
             </div>
